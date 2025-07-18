6085cd73e59729ff614ea989bcca48f508f1aa9e
Ashley Laisure <ashley.laisure@gmail.com>
2025-07-18 17:14:31 -0400

    day 2

--- components/main-header/Header.jsx
+++ components/main-header/Header.jsx
@@ -1,26 +1,34 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
 import logoImg from '@/assets/logo.png'
 import classes from './Header.module.css'
+import HeaderBackground from './Header-background'
+import NavLink from './nav-link'
 
 const Header = () => {
     return (
+        <>
+        <HeaderBackground />
+        
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
-                <img src={logoImg.src} alt="A plate with food" />
+                <Image src={logoImg}  alt="A plate with food" priority/>
                 NextLevel Food
             </Link>
 
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Browse meals</Link>
+                        <NavLink href="/meals">Browse Meals</NavLink>
                     </li>
                     <li>
-                        <Link href="/community">Foodies Community</Link>
+                        <NavLink href="/community">Foodies Community</NavLink>
                     </li>
                 </ul>
             </nav>
         </header>
+        </>
     )
 }
 
